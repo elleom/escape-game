295530340b07fab6d8dbdc9c0fbb1ce1e1f51dd3
0elle0 <el_leo_m@hotmail.com>
2021-03-20 23:05:03 +0100

    kyeboard listeners

--- listing1-3.py
+++ listing1-3.py
@@ -7,6 +7,23 @@ player_x = 600
 player_y = 350
 
 def draw():
-    screen.blit(images.backdrop, (0,0))
-    screen.blit(images.ship, (130, 150))
+    screen.blit(images.backdrop, (0,0))  # X, Y from top left => down right
     screen.blit(images.mars, (50, 50))
+    screen.blit(images.astronaut, (player_x, player_y))
+    screen.blit(images.ship, (550, 300))
+
+def game_loop():
+    global player_x, player_y
+
+    # keyboard listeners
+
+    if keyboard.right:
+        player_x += 5
+    elif keyboard.left:
+        player_x -= 5
+    elif keyboard.up:
+        player_y -= 5
+    elif keyboard.down:
+        player_y += 5
+
+clock.schedule_interval(game_loop, 0.03)
