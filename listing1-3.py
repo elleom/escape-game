7d5e04a6e65f4ecf0566f67db91fd965fb273e2c
0elle0 <el_leo_m@hotmail.com>
2021-03-20 23:39:23 +0100

    refactor

--- listing1-3.py
+++ listing1-3.py
@@ -1,16 +1,21 @@
 # Spacewalk
 
+import pygame
+
 WIDTH = 800
 HEIGHT = 600
 
 player_x = 600
 player_y = 350
 
+ship_x = 550
+ship_y = 300
+
 def draw():
     screen.blit(images.backdrop, (0,0))  # X, Y from top left => down right
     screen.blit(images.mars, (50, 50))
     screen.blit(images.astronaut, (player_x, player_y))
-    screen.blit(images.ship, (550, 300))
+    screen.blit(images.ship, (ship_x, ship_y))
 
 def game_loop():
     global player_x, player_y
